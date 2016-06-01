a2e49a1da0211efcaa5aa58cebba3fab2b58a3b7
Demitryh <dmytro.haras@gmail.com>
2016-06-01 18:58:36 +0300

    Add files via upload

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,19 +1,21 @@
 function addRow() {
 
-    var table = document.getElementById("example");
+    var table = document.createElement('table');
+    document.body.appendChild(table);
     var test = document.getElementById('input').files[0];
+    var k = document.getElementById("num").value;
     Papa.parse(test, {
         complete:function(results) {
             data=results.data;
             for (var i=0;i<=data.length;i++) {
                 var newrow = table.insertRow(-1);
 
-                for (var j = 0; j <= 7; j++) {
+                for (var j = 0; j != k; j++) {
                     newrow.insertCell(-1).innerHTML = data[i][j];
                 }
             }
 
         }
     });
-
 }
+
