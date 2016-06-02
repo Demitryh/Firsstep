0675ff7cfc54a0085c67884c93ed5079d0afcf34
Demitryh <dmytro.haras@gmail.com>
2016-06-02 15:35:53 +0300

    Add files via upload

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,21 +1,20 @@
 
 
-var but = document.getElementById("button");
+
 function addRow() {
     var table = document.createElement("table");
     document.body.appendChild(table);
     var input = document.getElementById("input").files[0];
-    var k = document.getElementById("num").value;
 
     Papa.parse(input, {
         complete:function(results) {
-            data=results.data;
-
+            var data = results.data;
+            console.log(results);
 
             for (var i=0;i<=data.length;i++) {
                 var newrow = table.insertRow(-1);
 
-                for (var j = 0; j != k; j++) {
+                for (var j = 0; j !=data[1].length; j++) {
                     newrow.insertCell(-1).innerHTML = data[i][j];
                 }
             }
