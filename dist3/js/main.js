27294d1a393ec50c72da36013387b3cdcfb638d6
Demitryh <dmytro.haras@gmail.com>
2016-06-05 18:16:38 +0300

    Add files via upload

--- dist3/js/main.js
+++ dist3/js/main.js
@@ -5,37 +5,39 @@ document.body.appendChild(table);
 
 document.getElementById("table").style.display = "none";
 window.onload =function () {
-
-
     document.getElementById("butt").style.display = "none";
     document.getElementById("inputInfo").style.display = "none";
-}
+};
 var insertRow= table.insertRow(-1);
 insertRow.insertCell(-1).innerHTML=('ФИО');
 insertRow.insertCell(-1).innerHTML=('Статус');
 insertRow.insertCell(-1).innerHTML=('Действия');
 
-var actionButton = '<button onclick="fire()"> уволить </button>';
-var dataBase ={
-    "name":'',
-    "status":true
-};
+
+
+var dataBase =[];
 function input() {
     document.getElementById("butt").style.display = "block";
     document.getElementById("inputInfo").style.display = "block";
+    document.getElementById('inputInfo').value='';
 }
 
 
-function tableCreator() {
+function tableCreator()
+{
     document.getElementById("table").style.display = "table";
     insertRow= table.insertRow(-1);
     var info = document.getElementById('inputInfo').value;
-    dataBase['name'] = info;
-    console.log(dataBase);
+    var actionButton = '<button onclick="fire()"> уволить </button>';
+    function Employee(name,status) {
+        this.name = info;
+        this.status=true;
 
-
+    }
+    dataBase = new Employee(info,true);
     insertRow.insertCell(-1).innerHTML=(dataBase['name']);
-    if (dataBase['status'] = true) {
+    if (dataBase['status'] = true)
+    {
         insertRow.insertCell(-1).innerHTML = ('Работает');
         insertRow.insertCell(-1).innerHTML=(actionButton);
     }
@@ -47,9 +49,13 @@ function tableCreator() {
     document.getElementById("butt").style.display = "none";
     document.getElementById("inputInfo").style.display = "none";
 
+    console.log(dataBase);
 }
 
 function fire() {
     dataBase['status']= false;
-    
+    insertRow.deleteCell(1);
+    insertRow.insertCell(1).innerHTML=("уволен");
+    insertRow.deleteCell(2);
+
 }
